eac55d92863111eb84ddbfef74e178d31a91f912
Dmitriy Fomin <79067773397@yandex.ru>
2022-01-05 23:34:30 +0300

    App was created

--- rsc/main/ru/fomin/App.java
+++ rsc/main/ru/fomin/App.java
@@ -2,11 +2,24 @@ package ru.fomin;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class App {
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         Connection connection = new SqlConnector().getConnection();
+        try {
+            connection.setAutoCommit(false);
+            Statement statement = connection.createStatement();
+            statement.execute("create table films (\n" +
+                    "id bigserial primary key,\n" +
+                    "name varchar(100) not null unique,\n" +
+                    "time_film interval not null)");
+
+            connection.commit();
+        } catch (Exception exception) {
+            connection.rollback();
+        }
     }
 
 }
