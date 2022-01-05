dc653b8a944425389b108ae45890bb14675b5581
Dmitriy Fomin <79067773397@yandex.ru>
2022-01-06 01:13:49 +0300

    App was updated

--- rsc/main/ru/fomin/App.java
+++ rsc/main/ru/fomin/App.java
@@ -7,15 +7,18 @@ import java.sql.Statement;
 public class App {
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        Connection connection = new SqlConnector().getConnection();
+        Connection connection = SqlConnector.getConnection();
         try {
             connection.setAutoCommit(false);
             Statement statement = connection.createStatement();
-            statement.execute("create table films (\n" +
+            statement.execute("CREATE TABLE IF NOT EXISTS films(\n" +
                     "id bigserial primary key,\n" +
                     "name varchar(100) not null unique,\n" +
-                    "time_film interval not null)");
-
+                    "time_film integer);");
+            statement.execute("create table IF NOT EXISTS prices (\n" +
+                    "id bigserial primary key,\n" +
+                    "cost bigint not null,\n" +
+                    "film_id bigint not null references films (id));");
             connection.commit();
         } catch (Exception exception) {
             connection.rollback();
