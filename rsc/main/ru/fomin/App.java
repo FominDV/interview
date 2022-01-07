f50c4cf0d306e381e87116c963e4fa310f92a2b4
Dmitriy Fomin <79067773397@yandex.ru>
2022-01-07 21:00:26 +0300

    Query4 was created

--- rsc/main/ru/fomin/App.java
+++ rsc/main/ru/fomin/App.java
@@ -25,57 +25,57 @@ public class App {
                     "sold boolean,\n" +
                     "session_id bigint not null references sessions (id));");
 
-//            PreparedStatement preparedStatement = connection.prepareStatement("Insert into films (name, time_film) values (?,?)");
-//            preparedStatement.setString(1, "Rambo");
-//            preparedStatement.setInt(2, 60);
-//            preparedStatement.addBatch();
-//            preparedStatement.setString(1, "Horror");
-//            preparedStatement.setInt(2, 60);
-//            preparedStatement.addBatch();
-//            preparedStatement.setString(1, "Blade");
-//            preparedStatement.setInt(2, 90);
-//            preparedStatement.addBatch();
-//            preparedStatement.setString(1, "Lord of the ring");
-//            preparedStatement.setInt(2, 120);
-//            preparedStatement.addBatch();
-//            preparedStatement.setString(1, "Remember all");
-//            preparedStatement.setInt(2, 120);
-//            preparedStatement.addBatch();
-//            preparedStatement.executeBatch();
-//
-  //       PreparedStatement   preparedStatement = connection.prepareStatement("Insert into sessions (date, film_id) values (?,?)");
-//            preparedStatement.setTimestamp(1, Timestamp.valueOf(LocalDateTime.now()));
-//            preparedStatement.setLong(2, 1);
-//            preparedStatement.addBatch();
-    //        preparedStatement.setTimestamp(1, Timestamp.valueOf(LocalDateTime.now().minusMinutes(65)));
-    //        preparedStatement.setLong(2, 2);
-     //       preparedStatement.addBatch();
-//            preparedStatement.setTimestamp(1, Timestamp.valueOf(LocalDateTime.now().minusMinutes(67)));
-//            preparedStatement.setLong(2, 3);
-//            preparedStatement.addBatch();
-//            preparedStatement.setTimestamp(1, Timestamp.valueOf(LocalDateTime.now().plusHours(1)));
-//            preparedStatement.setLong(2, 4);
-//            preparedStatement.addBatch();
-//            preparedStatement.setTimestamp(1, Timestamp.valueOf(LocalDateTime.now().plusMinutes(45)));
-//            preparedStatement.setLong(2, 5);
-//            preparedStatement.addBatch();
-//            preparedStatement.executeBatch();
-//
-//
-//            preparedStatement = connection.prepareStatement("Insert into tickets (price, sold, session_id) values (?,?,?)");
-//            preparedStatement.setLong(1, 600);
-//            preparedStatement.setBoolean(2, true);
-//            preparedStatement.setLong(3, 1);
-//            preparedStatement.addBatch();
-//            preparedStatement.setLong(1, 450);
-//            preparedStatement.setBoolean(2, true);
-//            preparedStatement.setLong(3, 2);
-//            preparedStatement.addBatch();
-//            preparedStatement.setLong(1, 300);
-//            preparedStatement.setBoolean(2, true);
-//            preparedStatement.setLong(3, 4);
-//            preparedStatement.addBatch();
-//            preparedStatement.executeBatch();
+            PreparedStatement preparedStatement = connection.prepareStatement("Insert into films (name, time_film) values (?,?)");
+            preparedStatement.setString(1, "Rambo");
+            preparedStatement.setInt(2, 60);
+            preparedStatement.addBatch();
+            preparedStatement.setString(1, "Horror");
+            preparedStatement.setInt(2, 60);
+            preparedStatement.addBatch();
+            preparedStatement.setString(1, "Blade");
+            preparedStatement.setInt(2, 90);
+            preparedStatement.addBatch();
+            preparedStatement.setString(1, "Lord of the ring");
+            preparedStatement.setInt(2, 120);
+            preparedStatement.addBatch();
+            preparedStatement.setString(1, "Remember all");
+            preparedStatement.setInt(2, 120);
+            preparedStatement.addBatch();
+            preparedStatement.executeBatch();
+
+            preparedStatement = connection.prepareStatement("Insert into sessions (date, film_id) values (?,?)");
+            preparedStatement.setTimestamp(1, Timestamp.valueOf(LocalDateTime.now()));
+            preparedStatement.setLong(2, 1);
+            preparedStatement.addBatch();
+            preparedStatement.setTimestamp(1, Timestamp.valueOf(LocalDateTime.now().minusMinutes(65)));
+            preparedStatement.setLong(2, 2);
+            preparedStatement.addBatch();
+            preparedStatement.setTimestamp(1, Timestamp.valueOf(LocalDateTime.now().minusMinutes(67)));
+            preparedStatement.setLong(2, 3);
+            preparedStatement.addBatch();
+            preparedStatement.setTimestamp(1, Timestamp.valueOf(LocalDateTime.now().plusHours(1)));
+            preparedStatement.setLong(2, 4);
+            preparedStatement.addBatch();
+            preparedStatement.setTimestamp(1, Timestamp.valueOf(LocalDateTime.now().plusMinutes(45)));
+            preparedStatement.setLong(2, 5);
+            preparedStatement.addBatch();
+            preparedStatement.executeBatch();
+
+
+            preparedStatement = connection.prepareStatement("Insert into tickets (price, sold, session_id) values (?,?,?)");
+            preparedStatement.setLong(1, 600);
+            preparedStatement.setBoolean(2, true);
+            preparedStatement.setLong(3, 1);
+            preparedStatement.addBatch();
+            preparedStatement.setLong(1, 450);
+            preparedStatement.setBoolean(2, true);
+            preparedStatement.setLong(3, 2);
+            preparedStatement.addBatch();
+            preparedStatement.setLong(1, 300);
+            preparedStatement.setBoolean(2, true);
+            preparedStatement.setLong(3, 4);
+            preparedStatement.addBatch();
+            preparedStatement.executeBatch();
 
             connection.commit();
         } catch (Exception exception) {
@@ -88,6 +88,7 @@ public class App {
         System.out.println();
         Query3.doIt(statement);
         System.out.println();
+        Query4.doIt(statement);
     }
 
 }
